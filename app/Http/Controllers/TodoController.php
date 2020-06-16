65d604ed5eb2505a11c692a4a7f9d4f437d4fe78
maaaaac5 <c5yuuuu0425@gmail.com>
2020-06-16 14:15:53 +0900

    TodoControllerのリダイレクトをルートネームを使用した処理に変更

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -49,7 +49,7 @@ class TodoController extends Controller
         $input = $request->all();
         $input['user_id'] = Auth::id(); 
         $this->todo->fill($input)->save();
-        return redirect()->to('todo');
+        return redirect()->route('todo.index');
     }
 
     /**
@@ -91,7 +91,7 @@ class TodoController extends Controller
         // dd($input);
         // dd($this->todo->find($id)->fill($input));
         $this->todo->find($id)->fill($input)->save();
-        return redirect()->to('todo');
+        return redirect()->route('todo.index');
     }
 
     /**
@@ -103,6 +103,6 @@ class TodoController extends Controller
     public function destroy($id)
     {
         $this->todo->find($id)->delete();
-        return redirect()->to('todo');
+        return redirect()->route('todo.index');
     }
 }
